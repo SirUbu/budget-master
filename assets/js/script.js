84a3b0748095bfa3d6c703b38cbd7a2b62a42b0b
Sir Ubu <TheSirUbu@gmail.com>
2020-09-09 10:05:49 -0600

    Updated pseudocode outline.

---
+++ assets/js/script.js
@@ -0,0 +1,96 @@
+// get DOM elements
+
+
+// set global variable 
+var todayDate = moment().format("MM/DD/YYYY");
+  //variables from set pay frequency function
+var payFrequency = {};
+    // {type: semi-monthly,
+    //  day1: "5",
+    //  day2: "22"}
+    // OR
+    // {type: bi-weekly,
+    // recent: "09/04/2020"}
+  // variables from set expenses function 
+var expenses = [
+    // {1: [
+        // {name: "",
+        // description: "",
+        // amount: "",
+        // status: false}
+    // ]}
+    {1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [],
+    12: [],
+    13: [],
+    14: [],
+    15: [],
+    16: [],
+    17: [],
+    18: [],
+    19: [],
+    20: [],
+    21: [],
+    22: [],
+    23: [],
+    24: [],
+    25: [],
+    26: [],
+    27: [],
+    28: [],
+    29: [],
+    30: [],
+    31: [],
+}];
+  // variable for pay period expenses
+var payPeriodExpenses = [];
+  // variables for calculator
+var balance = "";
+var expenses = "";
+var paid = "";
+var outstanding = "";
+var remaining = "";
+
+
+// functions
+    // function to fetch and display quote
+
+
+    // function to set pay frequency
+
+
+    // function to set expenses
+
+
+    // function to fetch holidays 
+    
+
+    // function to generate and display calendar
+
+
+    // function to calculate and display in calculator
+
+
+    // function to save localStorage
+
+
+    // function to get localStorage
+
+
+// call functions
+    // get localStorage
+
+
+    // calendar generation/display
+
+
+// event listeners
