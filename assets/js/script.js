5b10eeb3e76cb8b9525b8e45486dfbc8e07eeaab
Sir Ubu <TheSirUbu@gmail.com>
2020-09-10 13:49:05 -0600

    Calendar function creates dates with expenses.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 // get DOM elements
-var calDates = document.querySelector("#calGrid");
+var calDatesEl = document.querySelector("#calGrid");
+var cardContentEl = document.querySelector("#calBody");
+var yearEl = document.querySelector("#year");
+var monthEl = document.querySelector("#month");
 
 
 // set global variable 
 var todayDate = moment().format("MM/DD/YYYY");
   //variables from set pay frequency function
 var payFrequency = {};
-    // {type: semi-monthly,
+    // {type: "semi-monthly",
     //  day1: "5th",
     //  day2: "22nd"}
     // OR
-    // {type: bi-weekly,
+    // {type: "bi-weekly",
     // recent: "09/04/2020"}
   // variables from set expenses function 
 var expenses = [
@@ -19,11 +22,11 @@ var expenses = [
     {day: "2nd", expenseList: []},
     {day: "3rd", expenseList: []},
     {day: "4th", expenseList: []},
-    {day: "5th", expenseList: []},
+    {day: "5th", expenseList: [{name: "Verizon", description: "Phone Bill", amount: "$80", status: false}]},
     {day: "6th", expenseList: []},
     {day: "7th", expenseList: []},
     {day: "8th", expenseList: []},
-    {day: "9th", expenseList: [{name: "Wells Fargo", description: "Credit Card", amount: "$25", status: false}]},
+    {day: "9th", expenseList: [{name: "Wells Fargo", description: "Credit Card", amount: "$25", status: false},{name: "Rent", description: "Rent", amount: "$500", status: false}]},
     {day: "10th", expenseList: []},
     {day: "11th", expenseList: []},
     {day: "12th", expenseList: []},
@@ -33,7 +36,7 @@ var expenses = [
     {day: "16th", expenseList: []},
     {day: "17th", expenseList: []},
     {day: "18th", expenseList: []},
-    {day: "19th", expenseList: []},
+    {day: "19th", expenseList: [{name: "Car Loan", description: "Mountain America", amount: "$250", status: false},]},
     {day: "20th", expenseList: []},
     {day: "21st", expenseList: []},
     {day: "22nd", expenseList: []},
@@ -72,40 +75,47 @@ var calRemaining = "";
 
     // function to generate and display calendar
     var createCalendar = function () {
-      // determine date with note ie 1st, 2nd, 3rd
-      for(var i = 0; i < 31; i++) {
-        var dateNote = "";
-          if(i+1 === 1 || i+1 === 21 || i+1 === 31) {
-            dateNote = "st";
-          } else if(i+1 === 2 || i+1 == 22) {
-            dateNote = "nd";
-          } else if(i+1 === 3 || i+1 === 23) {
-            dateNote = "rd";
-          } else {
-            dateNote = "th";
-          };
-        var date = `${i+1}${dateNote}`;
+      // update month and year with current month and year
+      yearEl.textContent = moment().format("YYYY");
+      monthEl.textContent = moment().format("MMMM");
+      for(var i = 0; i < expenses.length; i++) {
         // create card for date and content
         var dateEl = document.createElement("div");
         dateEl.classList = "card";
         // create and add date as card header
-        var dateHeader = document.createElement("span");
+        var dateHeader = document.createElement("div");
         dateHeader.classList = "card-title left-align grey";
-        dateHeader.textContent = date;
+        dateHeader.textContent = expenses[i].day;
         dateEl.appendChild(dateHeader);
         // create and add card body
-        var dateBody = document.createElement("span");
-        dateBody.classList = "card-content";
+        var dateBody = document.createElement("div");
+        dateBody.classList = "card-body";
           // add pay days to card-body
-  
-          // add expenses to card-body
-        for(var d = 0; d < expenses.length; d++) {
-          if(expenses[i] === "9th") {
-            console.log(expenses[i]);
+        if(payFrequency.type === "semi-monthly") {
+          if(expenses[i].day === payFrequency.day1 || expenses[i].day === payFrequency.day2) {
+            var payEl = document.createElement("p");
+            payEl.classList = "green";
+            payEl.textContent = "Pay Day";
+            dateBody.appendChild(payEl);
+          }
+        } else if(payFrequency.type === "bi-weekly") {
+          var nextPay = payFrequency.recent
+          if(expenses[i].day === payFrequency.recent) {
+            var payEl = document.createElement("p");
+            payEl.classList = "green";
+            payEl.textContent = "Pay Day";
+            dateBody.appendChild(payEl);
           }
         }
+          // add expenses to card-body
+        for(var e = 0; e < expenses[i].expenseList.length; e++) {
+          var expenseEl = document.createElement("p");
+          expenseEl.textContent = expenses[i].expenseList[e].name;
+          expenseEl.classList = "red";
+          dateBody.appendChild(expenseEl);
+        }
         dateEl.appendChild(dateBody);
-        calDates.appendChild(dateEl);
+        calDatesEl.appendChild(dateEl);
       }
     };
 
